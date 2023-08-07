414dd06c0289acd41f4fb43c57595395722a228f
Pia <76558220+rkdud007@users.noreply.github.com>
2023-08-07 12:06:45 +0800

    noob starknet

--- noob_blockchain/src/main.rs
+++ noob_blockchain/src/main.rs
@@ -1,44 +1,128 @@
 use chrono::{DateTime, Utc};
 use sha2::{Digest, Sha256};
+use starknet::{
+    core::{crypto::compute_hash_on_elements, types::FieldElement},
+    providers::sequencer::models::{ConfirmedTransactionReceipt, TransactionType},
+};
 
-#[derive(Clone)]
-struct Block {
-    index: u32,
-    timestamp: DateTime<Utc>,
-    hash: String,
-    parent_hash: String,
-    data: String,
+#[derive(Debug, Clone)]
+struct BlockHeader {
+    parent_block_hash: FieldElement,
+    block_number: u64,
+    global_state_root: FieldElement,
+    sequencer_address: FieldElement,
+    block_timestamp: u64,
+    transaction_count: u64,
+    transaction_commitment: FieldElement,
+    event_count: u64,
+    event_commitment: FieldElement,
+    protocol_version: u64,
+    gas_price: FieldElement,
 }
 
-impl Block {
-    fn new(index: u32, timestamp: DateTime<Utc>, parent_hash: String, data: String) -> Self {
-        let hash = Self::compute_hash(index, timestamp, &parent_hash, &data);
-        Block {
-            index,
-            timestamp,
-            hash,
-            parent_hash,
-            data,
+impl BlockHeader {
+    fn new(
+        parent_block_hash: FieldElement,
+        block_number: u64,
+        global_state_root: FieldElement,
+        sequencer_address: FieldElement,
+        block_timestamp: u64,
+        transaction_count: u64,
+        transaction_commitment: FieldElement,
+        event_count: u64,
+        event_commitment: FieldElement,
+        protocol_version: u64,
+        gas_price: FieldElement,
+    ) -> Self {
+        Self {
+            parent_block_hash,
+            block_number,
+            global_state_root,
+            sequencer_address,
+            block_timestamp,
+            transaction_count,
+            transaction_commitment,
+            event_count,
+            event_commitment,
+            protocol_version,
+            gas_price,
         }
     }
 
-    fn compute_hash(index: u32, timestamp: DateTime<Utc>, parent_hash: &str, data: &str) -> String {
-        let input = format!("{}{}{}{}", index, timestamp, data, parent_hash);
-        let mut hasher = Sha256::new();
-        hasher.update(input);
-        let result = hasher.finalize();
-        format!("{:x}", result)
+    fn hash(&self) -> FieldElement {
+        compute_hash_on_elements(&vec![
+            self.block_number.into(),
+            self.global_state_root,
+            self.sequencer_address,
+            self.block_timestamp.into(),
+            self.transaction_count.into(),
+            self.transaction_commitment,
+            self.event_count.into(),
+            self.event_commitment,
+            FieldElement::ZERO,
+            FieldElement::ZERO,
+            self.protocol_version.into(),
+        ])
     }
 }
 
+#[derive(Debug, Clone)]
+struct Block {
+    header: BlockHeader,
+    transactions: Vec<FieldElement>,
+}
+
 fn create_genesis_block() -> Block {
-    Block::new(0, Utc::now(), "0".to_owned(), "Genesis Block".to_owned())
+    Block {
+        header: BlockHeader::new(
+            FieldElement::ZERO,
+            0,
+            FieldElement::ZERO,
+            FieldElement::ZERO,
+            0,
+            0,
+            FieldElement::ZERO,
+            0,
+            FieldElement::ZERO,
+            0,
+            FieldElement::ZERO,
+        ),
+        transactions: vec![],
+    }
 }
 fn create_new_block(prev_block: &Block, data: String) -> Block {
-    let index = prev_block.index + 1;
-    let timestamp = Utc::now();
-    let parent_hash = prev_block.hash.clone();
-    Block::new(index, timestamp, parent_hash, data)
+    let prev_block_hash = prev_block.header.hash();
+    let block_number = prev_block.header.block_number + 1;
+    let global_state_root = FieldElement::ZERO;
+    let sequencer_address = FieldElement::ZERO;
+    let block_timestamp = Utc::now().timestamp() as u64;
+    let transaction_count = 0;
+    let transaction_commitment = FieldElement::ZERO;
+    let event_count = 0;
+    let event_commitment = FieldElement::ZERO;
+    let protocol_version = 0;
+    let gas_price = FieldElement::ZERO;
+
+    let header = BlockHeader::new(
+        prev_block_hash,
+        block_number,
+        global_state_root,
+        sequencer_address,
+        block_timestamp,
+        transaction_count,
+        transaction_commitment,
+        event_count,
+        event_commitment,
+        protocol_version,
+        gas_price,
+    );
+
+    let transactions = vec![];
+
+    Block {
+        header,
+        transactions,
+    }
 }
 
 fn main() {
@@ -51,10 +135,10 @@ fn main() {
         let new_block_data = format!("Block #{} data.", i);
         let new_block = create_new_block(&prev_block, new_block_data);
         println!(
-            "Block #{} has been added to the blockchain!, {}, {}",
-            new_block.index, new_block.timestamp, new_block.data
+            "Block #{} has been added to the blockchain!, {}",
+            new_block.header.block_number, new_block.header.block_timestamp
         );
-        println!("Hash: {}\n", new_block.hash);
+        println!("Hash: {}\n", new_block.header.hash());
         blockchain.push(new_block.clone());
         prev_block = new_block;
     }
